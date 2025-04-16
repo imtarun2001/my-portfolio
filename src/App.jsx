2827d841883724b0ce94aaf0cf9ccbb11f32c95d
Tarun Kumar Behera <tarunkumarbehera8249@gmail.com>
2025-04-16 09:24:51 +0530

    added skills and changed some background color

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Skills from "./components/Skills"
 function App() {
 
   return (
-    <div className="w-screen bg-black text-white flex flex-col justify-start items-center gap-10 font-bold min-h-screen py-2.5">
+    <div className="w-screen bg-black text-white flex flex-col justify-start items-center gap-10 font-bold min-h-screen">
 
       <Details></Details>
 
