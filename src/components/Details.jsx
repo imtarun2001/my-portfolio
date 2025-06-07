629dafee7de35492b4a1cd394610d109a80794dd
Tarun Kumar Behera <tarunkumarbehera8249@gmail.com>
2025-06-07 22:12:08 +0530

    animations added

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,6 +7,7 @@ import { IoMdMail } from "react-icons/io";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { FaChevronUp,FaChevronDown } from "react-icons/fa";
 import { useAppContext } from '../context/AppContext';
+import { motion } from 'framer-motion';
 
 const Details = () => {
 
@@ -16,13 +17,15 @@ const Details = () => {
 
   const arrOfHeaders = ['projects','skills','certificates','academic'];
 
+  const transition = {type: 'normal', duration: 1};
+
   return (
     <div className="details w-full sm:h-screen p-[1rem] bg-gray-100 text-black tracking-wide flex flex-col justify-start sm:justify-between items-center gap-[1rem] sm:gap-0">
 
       {/* 1st section */}
       <div className='w-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-[2rem] md:gap-0'>
-        <div className='text-[2rem] bg-white px-[1rem] drop-shadow-lg drop-shadow-black'>PORT<span className='text-purple-700'>FOLIO</span></div>
-        <div className='flex justify-center uppercase items-center gap-[1.2rem] sm:gap-[2.5rem]'>
+        <motion.div  initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={transition}  className='text-[2rem] bg-white px-[1rem] drop-shadow-lg drop-shadow-black'>PORT<span className='text-purple-700'>FOLIO</span></motion.div>
+        <motion.div initial={{opacity:0,y:100}} whileInView={{opacity:1,y:0}} transition={transition}  className='flex justify-center uppercase items-center gap-[1.2rem] sm:gap-[2.5rem]'>
           {
             arrOfHeaders.map((header,index) => (
               <div key={index} className='hover:text-purple-700 text-[0.7rem] sm:text-[1rem] cursor-pointer'>
@@ -33,25 +36,25 @@ const Details = () => {
               </div>
             ))
           }
-        </div>
-        <div>
+        </motion.div>
+        <motion.div  initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={transition} >
           <a href="Tarun_Kumar_Behera_Resume_MERN_2025.pdf" download="Tarun_Kumar_Behera_Resume_MERN_2025.pdf">
             <button className='flex justify-center items-center gap-[0.5rem] px-[1.2rem] py-[0.6rem] bg-purple-700 text-white font-semibold rounded-lg cursor-pointer drop-shadow-md drop-shadow-black'>Download CV <MdOutlineFileDownload className='text-2xl'/></button>
           </a>
-        </div>
+        </motion.div>
       </div>
 
 
       {/* 2nd section */}
       <div className='w-full flex flex-col sm:flex-row justify-center items-center'>
         <div className='relative w-full sm:w-1/3 flex justify-center items-center'>
-          <img src={details.photo} alt="" className='w-1/2 sm:w-2/3 z-10 translate-y-1 sm:translate-y-2'/>
-          <div className='h-[70%] rounded-t-full bg-purple-700 w-[45%] sm:w-[55%] absolute bottom-0'></div>
-          <div className='h-[60%] rounded-t-full bg-black w-[35%] sm:w-[45%] absolute bottom-0'></div>
+          <motion.img  initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={transition}img src={details.photo} alt="" className='w-1/2 sm:w-2/3 z-10 translate-y-1 sm:translate-y-2'/>
+          <motion.div initial={{opacity:0,y:-100}} whileInView={{opacity:1,y:0}} transition={transition} className='h-[70%] rounded-t-full bg-purple-700 w-[45%] sm:w-[55%] absolute bottom-0'></motion.div>
+          <motion.div initial={{opacity:0,y:100}} whileInView={{opacity:1,y:0}} transition={transition}   className='h-[60%] rounded-t-full bg-black w-[35%] sm:w-[45%] absolute bottom-0'></motion.div>
         </div>
         <div className='w-full sm:w-2/3 flex flex-col justify-center sm:justify-start items-center'>
-          <div className='w-full text-center sm:text-justify text-[1rem]'>Hello Visitor 🙋‍♂️, I am</div>
-          <div className='w-full text-center sm:text-justify text-[2rem] sm:text-[3rem] text-purple-700'>{details.name}</div>
+          <motion.div  initial={{opacity:0}} whileInView={{opacity:1}} transition={transition} className='w-full text-center sm:text-justify text-[1rem]'>Hello Visitor 🙋‍♂️, I am</motion.div>
+          <motion.div  initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={transition} className='w-full text-center sm:text-justify text-[2rem] sm:text-[3rem] text-purple-700'>{details.name}</motion.div>
           <div className='w-full text-center sm:text-justify text-[1rem] flex justify-start items-center gap-[0.5rem]'>
             <div>and I am a</div>
             <div className='text-emerald-600 text-[1.1rem] sm:text-[1.7rem] text-nowrap'>
@@ -75,11 +78,13 @@ const Details = () => {
           <div className='flex justify-center items-center text-center gap-[0.2rem] text-[1rem]'><MdCall/>+91 9249390690</div>
           <div className='flex justify-center items-center text-center gap-[0.2rem] text-[1rem]'><IoMdMail/>tarunkumarbehera8249@gmail.com</div>
         </div>
-
-
         <div className='w-full px-[1rem] py-[0.5rem] flex justify-between items-center text-white'>
-          <button className='flex justify-center items-center gap-[0.4rem] px-[1.2rem] py-[0.6rem] bg-purple-700 font-bold rounded-lg transition duration-300 cursor-pointer drop-shadow-md drop-shadow-black' onClick={() => setShowup(prev => !prev)}>Contact me {showup ? <FaChevronUp/> : <FaChevronDown/> }</button>
-          <div className='flex justify-center items-center gap-[1rem] md:gap-[2rem] bg-purple-700 px-[1.2rem] py-[0.6rem] rounded-md text-[1.5rem]'>
+          <motion.div initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={transition}>
+            <button className='flex justify-center items-center gap-[0.4rem] px-[1.2rem] py-[0.6rem] bg-purple-700 font-bold rounded-lg transition duration-300 cursor-pointer drop-shadow-md drop-shadow-black' onClick={() => setShowup(prev => !prev)}>
+              Contact me {showup ? <FaChevronUp/> : <FaChevronDown/> }
+            </button>
+          </motion.div>
+          <motion.div  initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={transition} className='flex justify-center items-center gap-[1rem] md:gap-[2rem] bg-purple-700 px-[1.2rem] py-[0.6rem] rounded-md text-[1.5rem]'>
               <a href="https://www.linkedin.com/in/tarun-kumar-behera-228b3b242/" target='_blank'>
                 <FaLinkedin className='hover:bg-white hover:text-purple-700'/>
               </a>
@@ -89,7 +94,7 @@ const Details = () => {
               <a href="https://x.com/Tarunkb2001" target='_blank'>
                 <FaXTwitter className='hover:bg-white hover:text-purple-700 hover:rounded-md'/>
               </a>
-          </div>
+          </motion.div>
         </div>
       </div>
 
